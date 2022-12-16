652826f5ffca45da7949ca6e8a0ee343dc14dac3
Hiroki Yoshii <hiyosh@microsoft.com>
2022-12-16 13:30:15 -0600

    Git push 3

--- DotnetFunctionApp/Function1.cs
+++ DotnetFunctionApp/Function1.cs
@@ -11,7 +11,7 @@ namespace DotnetFunctionApp
         public void Run([TimerTrigger("0 */5 * * * *")]TimerInfo myTimer, ILogger log)
         {
             log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
-            log.LogInformation("Git push 2");
+            log.LogInformation("Git push 3");
         }
     }
 }
